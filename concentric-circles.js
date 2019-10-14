67195fd2898b2c19efa1f7875dd5ef0e0ef5fae5
chengjun334 <55414165+chengjun334@users.noreply.github.com>
2019-10-14 16:09:49 -0700

    Update concentric-circles.js

--- concentric-circles.js
+++ concentric-circles.js
@@ -4,9 +4,10 @@ function setup() {
 
 function draw() {
   background(255);
-  
+  noFill()
+
   for (var i = 10; i <= 390; i = i + 10) {
-  circle(200, 200, i);
-  
+    circle(200, 200, i);
+
   }
 }
